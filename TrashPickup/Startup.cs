0d63b0fe7405534711ee36d2082071d78d38d355
Ljiljana <dstoj1@yahoo.comgit config --global user.email dstoj1@yahoo.com>
2017-09-08 15:32:47 -0500

    Added RoleManager and UserManager in the startup

---
+++ TrashPickup/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using TrashPickup.Models;
+
+[assembly: OwinStartupAttribute(typeof(TrashPickup.Startup))]
+namespace TrashPickup
+{
+    public partial class Startup
+    {
+        private IRoleStore<IdentityRole, string> content;
+
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            createRoleandUser();
+        }
+        private void createRoleandUser()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!roleManager.RoleExists("Customer"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Customer";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Employee";
+                roleManager.Create(role);
+            }
+        }
+    }
+}
