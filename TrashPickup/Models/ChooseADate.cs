d7248395dfb16c946e9311a07e72056b7d964237
Ljiljana <dstoj1@yahoo.comgit config --global user.email dstoj1@yahoo.com>
2017-09-11 09:34:25 -0500

    Adding models and made changes to account controller

---
+++ TrashPickup/Models/ChooseADate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashPickup.Models
+{
+    public class ChooseADate
+    {
+        public ChooseADate()
+        {
+
+        }
+        private List<DateTime> getWeekDays(DateTime dt)
+        {
+            List<DateTime> result = new List<DateTime>();
+            int month = dt.Month;
+            dt = dt.AddDays(-dt.Day + 1);
+            if (dt.DayOfWeek == DayOfWeek.Saturday)
+            {
+                dt = dt.AddDays(2);
+            }
+            else if (dt.DayOfWeek == DayOfWeek.Sunday)
+            {
+                dt = dt.AddDays(1);
+            }
+            while (dt.Month == month)
+            {
+                result.Add(dt);
+                dt = dt.AddDays(dt.DayOfWeek == DayOfWeek.Friday ? 3 : 1);
+            }
+            return result;
+        }
+    }
+}
