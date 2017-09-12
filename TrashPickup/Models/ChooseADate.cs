27d7e9111c1afb3c524cca8a545488efba9f58bb
Ljiljana <dstoj1@yahoo.comgit config --global user.email dstoj1@yahoo.com>
2017-09-12 15:22:22 -0500

    Added Display info to ChooseDate

--- TrashPickup/Models/ChooseADate.cs
+++ TrashPickup/Models/ChooseADate.cs
@@ -7,30 +7,34 @@ namespace TrashPickup.Models
 {
     public class ChooseADate
     {
-        public ChooseADate()
-        {
+        [Display(Name = "Choose Another Day for Pick Up")]
+        public int AnotherDayForPickUp { get; set; }
+        [Display(Name = "Please Put Pick Up On Hold")]
+        public string PickUpOnHold { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
 
-        }
-        private List<DateTime> getWeekDays(DateTime WeekDays)
-        {
-            List<DateTime> result = new List<DateTime>();
-            int month = WeekDays.Month;
-            WeekDays = WeekDays.AddDays(-WeekDays.Day + 1);
-            if (WeekDays.DayOfWeek == DayOfWeek.Saturday)
-            {
-                WeekDays = WeekDays.AddDays(2);
-            }
-            else if (WeekDays.DayOfWeek == DayOfWeek.Sunday)
-            {
-                WeekDays = WeekDays.AddDays(1);
-            }
-            while (WeekDays.Month == month)
-            {
-                result.Add(WeekDays);
-                WeekDays = WeekDays.AddDays(WeekDays.DayOfWeek == DayOfWeek.Friday ? 3 : 1);
-            }
-            return result;
-        }
+        //private List<DateTime> getWeekDays(DateTime WeekDays)
+        //{
+        //    List<DateTime> result = new List<DateTime>();
+        //    int month = WeekDays.Month;
+        //    WeekDays = WeekDays.AddDays(-WeekDays.Day + 1);
+        //    if (WeekDays.DayOfWeek == DayOfWeek.Saturday)
+        //    {
+        //        WeekDays = WeekDays.AddDays(2);
+        //    }
+        //    else if (WeekDays.DayOfWeek == DayOfWeek.Sunday)
+        //    {
+        //        WeekDays = WeekDays.AddDays(1);
+        //    }
+        //    while (WeekDays.Month == month)
+        //    {
+        //        result.Add(WeekDays);
+        //        WeekDays = WeekDays.AddDays(WeekDays.DayOfWeek == DayOfWeek.Friday ? 3 : 1);
+        //    }
+        //    return result;
+        //}
     //     <div class="form-group">
     //    @Html.Label("user Role", new { @class = "col-md-2 control-label" })
     //    <div class="col-md-10">
@@ -43,5 +47,3 @@ namespace TrashPickup.Models
         //    <asp:DropDownList ID = "ddlDay" runat="server">
 
 //</asp:DropDownList>
-    }
-}
