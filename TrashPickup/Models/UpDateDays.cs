4b35afe2bdf59d914f885b6477ebeb7d597c4ed3
Ljiljana <dstoj1@yahoo.comgit config --global user.email dstoj1@yahoo.com>
2017-09-13 08:35:08 -0500

    Added Change day in Customer ChangeDay

--- TrashPickup/Models/UpDateDays.cs
+++ TrashPickup/Models/UpDateDays.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace TrashPickup.Models
 {
-    public class Days
+    public class UpDateDays
     {
+        [Display(Name = "Change Pick Up Day")]
+        public string ChangeDay { get; set; }
+        [Display(Name = "Hold/Start Service")]
+        public string HoldStart { get; set; }
     }
 }
